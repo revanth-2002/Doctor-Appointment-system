a19f0560c79f9b1c3044d77a3bb64da363b86914
revanth gaddam <gaddamrevanth11@gmail.com>
2023-08-10 12:26:13 +0530

    updated

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ connectDB();
 app.use("/api/v1/user",require("./routes/userRoutes"));
 app.use('/api/v1/admin',require("./routes/adminRoutes"));
 app.use('/api/v1/doctor',require("./routes/doctorRoutes"));
+
+//static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
+
 //listen port
 const port = process.env.PORT || 8080
 app.listen(port,()=>{
